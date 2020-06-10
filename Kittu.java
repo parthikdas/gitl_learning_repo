7fda557bd81bc695a741b2e645834b1f79ff7e8f
Kittu <kittu@MacBook-Pro.local>
2020-06-10 14:35:10 +0530

    this is branch a

--- Kittu.java
+++ Kittu.java
@@ -5,5 +5,6 @@ class Kittu
 	System.out.print("i have to learned to use git");
 	System.out.print("another change ");
 	System.out.print("new change is being done");
+	System.out.print("this text is written in branch a");
 	}
 }
